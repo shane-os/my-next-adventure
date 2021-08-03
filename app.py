bb7b37ce1434aef56dd16a486b517cf7e18de498
shane-os <shaneosullivansos@yahoo.co.uk>
2021-08-03 19:23:07 +0000

    Add new html files and routing

--- app.py
+++ app.py
@@ -12,6 +12,21 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/attractions")
+def attractions():
+    return render_template("attractions.html")
+
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+
+@app.route("/contactus")
+def contactus():
+    return render_template("contactus.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
